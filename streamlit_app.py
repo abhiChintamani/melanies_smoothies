364309874a4ddc83f38a5ae3f40f97f79d47dae5
Abhijit Chintamani <45992040+abhiChintamani@users.noreply.github.com>
2024-08-23 14:16:57 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ ingredients_list = st.multiselect(
     , max_selections=5
 )
 # st.write(my_dataframe)
-# pd_df = my_dataframe.to_pandas()
+pd_df = my_dataframe.to_pandas()
 # st.dataframe(pd_df)
 # st.write(pd_df)
 # st.stop()
@@ -45,6 +45,7 @@ if ingredients_list:
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write(search_on)
         
+        st.subheader(fruit_chosen + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
         
