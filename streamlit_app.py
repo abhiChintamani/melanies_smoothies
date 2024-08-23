3e06624e264c8bb390d1445009dd2805f6934abd
Abhijit Chintamani <45992040+abhiChintamani@users.noreply.github.com>
2024-08-23 13:32:13 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,12 @@ st.write("The name on your smoothie will be: ", title)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
 
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 
 ingredients_list = st.multiselect(
@@ -35,9 +38,14 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, 'is', search_on,'.')
+        
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
         
+
     my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '""" + title + """')"""
     TIME_TO_INSERT = st.button('Submit Order')
